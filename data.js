f37d8178494acdadf2983e9e5da0d7d48ee6972b
Subham Saha <103334624+subham132003@users.noreply.github.com>
2023-09-13 00:55:25 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+}, {
+    "id": 1,
+    "title": "Maruti Suzuki Swift",
+    "price": 499999,
+    "description": "Maruti Suzuki Swift price in India is Rs. 5,79,999. Official dealers and warranty providers regulate the retail price of Maruti Suzuki products in official warranty.",
+    "brand": "Maruti Suzuki",
+    "image": "http://i.ndtvimg.com/i/2016-04/maruti-suzuki-swift_827x510_71460371276.jpg"
+}, {
+    "id": 2,
+    "title": "Maruti Suzuki Ciaz",
+    "price": 1099999,
+    "description": "Maruti Suzuki Ciaz price in India is Rs. 15,99,999. Official dealers and warranty providers regulate the retail price of Maruti Suzuki products in official warranty.",
+    "brand": "Maruti Suzuki",
+    "image": "http://i.ndtvimg.com/i/2015-11/maruti-suzuki-ciaz-827_827x510_61447071611.jpg"
+}, {
+    "id": 3,
+    "title": "Datsun GO+",
+    "price": 699999,
+    "description": "Datsun GO+ price in India is Rs. 8,00,000. Official dealers and warranty providers regulate the retail price of Datsun products in official warranty.",
+    "brand": "Datsun",
+    "image": "http://i.ndtvimg.com/i/2014-12/datsun-go-plus-6_650x420_81418998774.jpg"
+}, {
+    "id": 4,
+    "title": "Land Rover Discovery Sport",
+    "price": 3699999,
+    "description": "Land Rover Discovery Sport price in India is Rs. 70,99,999. Official dealers and warranty providers regulate the retail price of Land Rover products in official warranty.",
+    "brand": "Land Rover",
+    "image": "http://i.ndtvimg.com/i/2016-08/land-rover-discovery-sport-off-road-experience_827x510_41470486633.jpg"
+}, {
+    "id": 5,
+    "title": "Volvo S60 Cross Country",
+    "price": 3549999,
+    "description": "Volvo S60 Cross Country price in India is Rs. 40,99,999. Official dealers and warranty providers regulate the retail price of Volvo products in official warranty.",
+    "brand": "Volvo",
+    "image": "http://i.ndtvimg.com/i/2016-03/volvo-s60-cross-country-827_827x510_51456830614.jpg"
+}, {
+    "id": 6,
+    "title": "Mercedes-Benz GLE Coupe",
+    "price": 7999999,
+    "description": "Mercedes-Benz price in India is Rs. 9000000. Official dealers and warranty providers regulate the retail price of Mercedes-Benz products in official warranty.",
+    "brand": "Mercedes-Benz",
+    "image": "http://i.ndtvimg.com/i/2015-12/mercedes-benz-gle-coupe_827x510_51449119963.jpg"
+}, {
+    "id": 7,
+    "title": "Audi A3 Cabriolet",
+    "price": 4999999,
+    "description": "Audi A3 Cabriolet price in India is Rs. 5500000. Official dealers and warranty providers regulate the retail price of Audi products in official warranty.",
+    "brand": "Audi",
+    "image": "http://i.ndtvimg.com/i/2014-12/audi-a3-cabriolet-3_625x300_51417497918.jpg"
+}]
